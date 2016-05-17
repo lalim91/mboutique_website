05beeba296e53d72b7ea473b91d2bdf4927a82dd
Leslie Ann Lim <Lalim@Leslies-MacBook-Air.local>
2016-05-16 23:12:36 -0700

    added routing

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-/**
- * Created by Lalim on 5/12/16.
- */
+var app = angular.module("mboutiqueApp", ['ngRoute']);
+
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl:"home.html",
+            controller:"mainController"
+        })
+        .when('/our-macarons',{
+            templateUrl:"macarons.html",
+            controller:"macaronsController"
+        })
+        .when('/gifts-parties',{
+            templateUrl:"gifts-parties.html",
+            contoller:"giftsController"
+        })
+        .when('/contact',{
+            templateUrl:"contact.html",
+            controller: "contactController"
+        })
+        .otherwise({
+            redirectTo:'/'
+        });
+
+});
+
+app.controller('mainController', function($scope){
+   $scope.banner = "aassets/images/welcome-imagae.png";
+}).controller('macaronsController', function($scope){
+    $scope.banner = "assets/images/our-macarons-image.png";
+}).controller('giftsController', function($scope){
+    $scope.banner = "assets/images/gifts-parties-image.png";
+}).controller('contactController', function($scope){
+    $scope.banner = "assets/images/contact-image.png";
+});
